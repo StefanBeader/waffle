1248cf184a0b80afbd6347ef31553194582dc582
vagrant <vagrant@homestead>
2019-02-16 13:45:10 +0000

    add user milan

---
+++ database/migrations/2019_02_16_133615_add_milan_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class AddMilanToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Milan',
+                'email' => 'wafflemagacin@gmail.com',
+                'password' => bcrypt('Waffle19!'),
+            ]
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('users')->where('email', 'wafflemagacin@gmail.com')->delete();
+    }
+}
